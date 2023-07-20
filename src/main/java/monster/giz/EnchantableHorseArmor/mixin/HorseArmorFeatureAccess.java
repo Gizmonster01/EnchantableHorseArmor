1b733bbadcba732216798d30be1885e0af1124e9
Gizmonster01 <unconfigured@null.spigotmc.org>
2023-07-20 19:12:57 -0400

    Deleted holdover
    Signed-off-by: Gizmonster01 <unconfigured@null.spigotmc.org>

--- src/main/java/monster/giz/EnchantableHorseArmor/mixin/HorseArmorFeatureAccess.java
+++
@@ -1,7 +0,0 @@
-package monster.giz.EnchantableHorseArmor.mixin;
-
-import net.minecraft.client.render.model.BakedModelManager;
-
-public interface HorseArmorFeatureAccess {
-    void defineAtlas(BakedModelManager bakery);
-}
