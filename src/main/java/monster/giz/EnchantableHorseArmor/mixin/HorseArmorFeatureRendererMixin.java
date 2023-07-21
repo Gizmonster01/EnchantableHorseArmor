9178d3fc4bc794c36d874bf7201e2346aaee02ae
gizmonster <jbaynard4@nc.rr.com>
2023-07-20 20:39:21 -0400

    Include new parameters and refactored trim retrieval method
    Signed-off-by: Gizmonster01 <unconfigured@null.spigotmc.org>

--- src/main/java/monster/giz/EnchantableHorseArmor/mixin/HorseArmorFeatureRendererMixin.java
+++ src/main/java/monster/giz/EnchantableHorseArmor/mixin/HorseArmorFeatureRendererMixin.java
@@ -1,8 +1,7 @@
 package monster.giz.EnchantableHorseArmor.mixin;
 
 import monster.giz.EnchantableHorseArmor.access.HorseArmorFeatureAccess;
-import monster.giz.EnchantableHorseArmor.util.EHALogger;
-import monster.giz.EnchantableHorseArmor.util.HorseArmorUtils;
+import monster.giz.EnchantableHorseArmor.util.HorseArmorTrimHelper;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.render.*;
@@ -18,7 +17,6 @@ import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.passive.HorseEntity;
 import net.minecraft.item.DyeableHorseArmorItem;
 import net.minecraft.item.HorseArmorItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.trim.ArmorTrim;
 import net.minecraft.util.Identifier;
@@ -43,47 +41,46 @@ public class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity,
      * @reason To be fully replaced with better practice once proof of concept is complete
      */
     @Overwrite
-    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int light, HorseEntity horseEntity, float f, float g, float h, float j, float k, float l) {
-        ItemStack itemStack = horseEntity.getArmorType();
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int light, HorseEntity horseEntity, float entityYaw, float partialTicks, float headPitch, float scale, float tickDelta, float animationProgress) {
+        ItemStack armorStack = horseEntity.getArmorType();
 
-        if (itemStack.getItem() instanceof HorseArmorItem) {
-            HorseArmorItem horseArmorItem = (HorseArmorItem) itemStack.getItem();
-            boolean glint = itemStack.hasGlint();
+        if (armorStack.getItem() instanceof HorseArmorItem) {
+            HorseArmorItem horseArmorItem = (HorseArmorItem) armorStack.getItem();
+            boolean hasGlint = armorStack.hasGlint();
 
             this.getContextModel().copyStateTo(this.model);
-            this.model.animateModel(horseEntity, f, g, h);
-            this.model.setAngles(horseEntity, f, g, j, k, l);
+            this.model.animateModel(horseEntity, entityYaw, partialTicks, headPitch);
+            this.model.setAngles(horseEntity, entityYaw, partialTicks, scale, tickDelta, animationProgress);
 
-            float red, blue, green;
+            float red, green, blue;
             if (horseArmorItem instanceof DyeableHorseArmorItem) {
-                int m = ((DyeableHorseArmorItem) horseArmorItem).getColor(itemStack);
-                red = (float) (m >> 16 & 255) / 255.0F;
-                blue = (float) (m >> 8 & 255) / 255.0F;
-                green = (float) (m & 255) / 255.0F;
+                int color = ((DyeableHorseArmorItem) horseArmorItem).getColor(armorStack);
+                red = (float) (color >> 16 & 255) / 255.0F;
+                green = (float) (color >> 8 & 255) / 255.0F;
+                blue = (float) (color & 255) / 255.0F;
             } else {
                 red = 1.0F;
-                blue = 1.0F;
                 green = 1.0F;
+                blue = 1.0F;
             }
 
             VertexConsumer vertexConsumer = ItemRenderer.getArmorGlintConsumer(
                     vertexConsumerProvider,
                     RenderLayer.getArmorCutoutNoCull(horseArmorItem.getEntityTexture()),
                     false,
-                    glint
+                    hasGlint
             );
 
-            this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, red, blue, green, 1.0F);
-            ArmorTrim.getTrim(horseEntity.getWorld().getRegistryManager(), itemStack).ifPresent((trim) -> {
-                this.renderTrim(itemStack.getItem(), matrices, vertexConsumerProvider, light, trim);
+            this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, red, green, blue, 1.0F);
+
+            ArmorTrim.getTrim(horseEntity.getWorld().getRegistryManager(), armorStack).ifPresent((trim) -> {
+                this.renderArmorTrim(horseArmorItem, matrices, vertexConsumerProvider, light, trim);
             });
         }
     }
 
-    private void renderTrim(Item item, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorTrim trim) {
-        //Sprite sprite = this.armorTrimsAtlas.getSprite(HorseArmorUtils.getHorseTrimIdentifier(trim, item));
-        Identifier trimIdentifier = HorseArmorUtils.getTestTrimIdentifier(item);
-        EHALogger.log("TESTING 123: " + trimIdentifier);
+    private void renderArmorTrim(HorseArmorItem item, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, ArmorTrim trim) {
+        Identifier trimIdentifier = HorseArmorTrimHelper.getTrimIdentifier(item, trim);
         Sprite sprite = this.armorTrimsAtlas.getSprite(trimIdentifier);
         VertexConsumer vertexConsumer = sprite.getTextureSpecificVertexConsumer(vertexConsumers.getBuffer(TexturedRenderLayers.getArmorTrims()));
         model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
