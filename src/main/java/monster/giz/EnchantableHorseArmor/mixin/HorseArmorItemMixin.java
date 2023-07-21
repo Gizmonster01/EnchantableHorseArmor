6def6d53c733fe7886edd2106de5e38340453b80
Gizmonster01 <unconfigured@null.spigotmc.org>
2023-07-20 20:37:02 -0400

    Added field for ArmorMaterial for trim override support. IE: iron trim with iron horse armor will now render iron_darker
    Signed-off-by: Gizmonster01 <unconfigured@null.spigotmc.org>

--- src/main/java/monster/giz/EnchantableHorseArmor/mixin/HorseArmorItemMixin.java
+++ src/main/java/monster/giz/EnchantableHorseArmor/mixin/HorseArmorItemMixin.java
@@ -1,12 +1,18 @@
 package monster.giz.EnchantableHorseArmor.mixin;
 
 
-import net.minecraft.item.*;
+import monster.giz.EnchantableHorseArmor.access.HorseArmorItemAccess;
+import net.minecraft.item.ArmorMaterial;
+import net.minecraft.item.HorseArmorItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 
 
 @Mixin(HorseArmorItem.class)
-public abstract class HorseArmorItemMixin extends Item {
+public abstract class HorseArmorItemMixin extends Item implements HorseArmorItemAccess {
+
+    private ArmorMaterial material;
 
     public HorseArmorItemMixin(Item.Settings settings) {
         super(settings);
@@ -21,5 +27,9 @@ public abstract class HorseArmorItemMixin extends Item {
     public int getEnchantability() {
         return 1;
     }
+
+    public void setMaterial(ArmorMaterial material) { this.material = material; }
+
+    public ArmorMaterial getMaterial() { return material; }
 
 }
