e18258eb688f5df0769869ef03afe1908254dc13
gizmonster <jbaynard4@nc.rr.com>
2023-09-29 23:43:11 -0400

    Changed mixin style to inject rather than overwrite for compatability reasons
    Signed-off-by: Gizmonster01 <unconfigured@null.spigotmc.org>

--- src/main/java/monster/giz/Overhorsed/mixin/HorseArmorFeatureRendererMixin.java
+++ src/main/java/monster/giz/Overhorsed/mixin/HorseArmorFeatureRendererMixin.java
@@ -17,18 +17,24 @@ import net.minecraft.client.texture.Sprite;
 import net.minecraft.client.texture.SpriteAtlasTexture;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.passive.HorseEntity;
-import net.minecraft.item.DyeableHorseArmorItem;
 import net.minecraft.item.HorseArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.trim.ArmorTrim;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import static net.minecraft.client.render.TexturedRenderLayers.ARMOR_TRIMS_ATLAS_TEXTURE;
 
 @Environment(EnvType.CLIENT)
 @Mixin(HorseArmorFeatureRenderer.class)
-public class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity, HorseEntityModel<HorseEntity>> implements HorseArmorFeatureAccess {
+public abstract class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity, HorseEntityModel<HorseEntity>> implements HorseArmorFeatureAccess {
     @Shadow @Final
     private HorseEntityModel<HorseEntity> model;
     @Unique
@@ -38,39 +44,14 @@ public class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity,
         super(context);
     }
 
-    /**
-     * @author gizmonster
-     * @reason To be fully replaced with better practice once proof of concept is complete
-     */
-    @Overwrite
-    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int light, HorseEntity horseEntity, float entityYaw, float partialTicks, float headPitch, float scale, float tickDelta, float animationProgress) {
-        ItemStack armorStack = horseEntity.getArmorType();
-        if (!(armorStack.getItem() instanceof HorseArmorItem horseArmorItem))
-            return;
-
-        boolean hasGlint = armorStack.hasGlint();
-
-        this.getContextModel().copyStateTo(this.model);
-        this.model.animateModel(horseEntity, entityYaw, partialTicks, headPitch);
-        this.model.setAngles(horseEntity, entityYaw, partialTicks, scale, tickDelta, animationProgress);
-
-        int color = 0xFFFFFF; // Default color (white)
-        if (horseArmorItem instanceof DyeableHorseArmorItem) {
-            color = ((DyeableHorseArmorItem) horseArmorItem).getColor(armorStack);
-        }
-
-        float red = (float) (color >> 16 & 255) / 255.0F;
-        float green = (float) (color >> 8 & 255) / 255.0F;
-        float blue = (float) (color & 255) / 255.0F;
-
-        // TODO: Modify consumer to support enchantment glint
-        VertexConsumer vertexConsumer = vertexConsumerProvider.getBuffer(RenderLayer.getEntityCutoutNoCull(horseArmorItem.getEntityTexture()));
-
-        this.model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, red, green, blue, 1.0F);
-
+    @Inject(method = "render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;ILnet/minecraft/entity/passive/HorseEntity;FFFFFF)V", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILHARD)
+    public void render(MatrixStack matrices, VertexConsumerProvider vertexConsumerProvider, int light, HorseEntity horseEntity, float f, float g, float h, float j, float k, float l, CallbackInfo ci, ItemStack armorStack, HorseArmorItem horseArmorItem, float n, float o, float p, VertexConsumer vertexConsumer) {
         ArmorTrim.getTrim(horseEntity.getWorld().getRegistryManager(), armorStack).ifPresent(trim -> {
             this.renderArmorTrim(horseArmorItem, matrices, vertexConsumerProvider, light, trim);
         });
+        if (armorStack.hasGlint()) {
+            this.renderGlint(matrices, vertexConsumerProvider, light);
+        }
     }
 
     @Unique
@@ -79,6 +60,10 @@ public class HorseArmorFeatureRendererMixin extends FeatureRenderer<HorseEntity,
         Sprite sprite = this.armorTrimsAtlas.getSprite(trimIdentifier);
         VertexConsumer vertexConsumer = sprite.getTextureSpecificVertexConsumer(vertexConsumers.getBuffer(RenderLayer.getEntityCutoutNoCull(ARMOR_TRIMS_ATLAS_TEXTURE, false)));
         model.render(matrices, vertexConsumer, light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
+    }
+
+    private void renderGlint(MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        model.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntityGlint()), light, OverlayTexture.DEFAULT_UV, 1.0F, 1.0F, 1.0F, 1.0F);
     }
 
     public void overhorsed$defineAtlas(BakedModelManager bakery) {
