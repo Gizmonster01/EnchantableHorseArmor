869145db3da0246a348e12e24a145545e345a058
gizmonster <jbaynard4@nc.rr.com>
2023-11-07 18:47:04 -0500

    Added logOnce, allows you to log something only once even when dropped in a spammy function
    Signed-off-by: Gizmonster01 <unconfigured@null.spigotmc.org>

--- src/main/java/monster/giz/Overhorsed/util/OHLogger.java
+++ src/main/java/monster/giz/Overhorsed/util/OHLogger.java
@@ -3,9 +3,20 @@ package monster.giz.Overhorsed.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class OHLogger {
 
     public static final Logger LOGGER = LoggerFactory.getLogger("overhorsed");
+    private static final Set<String> loggedOnce = new HashSet<>();
 
     public static void log(String string) { LOGGER.info(string); }
+
+    public static void logOnce(String string) {
+        if (!loggedOnce.contains(string)) {
+            LOGGER.info(string);
+            loggedOnce.add(string);
+        }
+    }
 }
